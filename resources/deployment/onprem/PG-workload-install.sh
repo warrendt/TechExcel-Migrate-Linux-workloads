fa177093b511f2cc13d4cedaa056bb4020569b98
HeyMo0sh <hamishwatson8@outlook.com>
2024-06-03 19:59:23 +1200

    Updates to PG install script

--- resources/deployment/onprem/PG-workload-install.sh
+++ resources/deployment/onprem/PG-workload-install.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+
 # Install PG
 sudo dnf module install postgresql:16/server -y
 
-# bring down git
-sudo yum install git -y
+# bring down necessary utilities
+sudo yum install git php php-pgsql httpd -y
+
+# setup apache
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
 # setup firewall rules
 sudo firewall-cmd --permanent --add-port=80/tcp
@@ -23,8 +29,6 @@ sudo -u postgres -i
 cd data
 sed -i "s/ident/trust/g" pg_hba.conf
 sed -i "s/peer/trust/g" pg_hba.conf
-sed -i "s/127.0.01\/32/0.0.0.0\/0/g"  pg_hba.conf
-
 sed -i "s/32/0/g"  pg_hba.conf
 sed -i "s/127/0/g"  pg_hba.conf
 sed -i '115s/\.1/\.0/;121s/\.1/\.0/' pg_hba.conf
@@ -33,14 +37,23 @@ sed -i "s/#port = 5432/port = 5432/g" postgresql.conf
 logout
 sudo systemctl restart postgresql.service
 
+#    
+sudo setsebool -P httpd_can_network_connect_db 1
+
+# bring down Northwind database
+sudo git clone https://github.com/HeyMo0sh/northwind_psql.git
+# sudo git clone git@github.com:pthom/northwind_psql.git
+
 # Create User and database
-sudo -u postgres -i
-psql
+sudo -u postgres psql -c 'create database northwind;'
+sudo -u postgres psql -c "CREATE USER demouser WITH PASSWORD 'demopass123';"
 
-CREATE USER demouser WITH PASSWORD 'demopass123' CREATEROLE CREATEDB;
-CREATE DATABASE mydatabase;
-GRANT ALL PRIVILEGES ON DATABASE "mydatabase" to demouser;
+# Populate database
+# bring down file to home and then sudo cp to /var/lib/pgsql
+sudo cp northwind_psql/northwind.sql /var/lib/pgsql
+sudo -u postgres psql -d northwind -a -f /var/lib/pgsql/northwind.sql
+sudo -u postgres psql -d northwind -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO demouser;"
+sudo -u postgres psql -d northwind -c "GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO demouser;"
 
-# Start Apache
-sudo systemctl start httpd
-sudo systemctl enable httpd
+# Copy in the php file
+sudo cp northwind_psql/orders.php /var/www/html
